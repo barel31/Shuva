06b1930e207522593cc94c771367e6d7192f5ddb
barel31 <barel123133@gmail.com>
2024-07-31 17:46:33 +0300

    Added some icons

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {unsplashImageAsset} from 'sanity-plugin-asset-source-unsplash'
 
 export default defineConfig({
   name: 'default',
@@ -9,7 +10,7 @@ export default defineConfig({
   projectId: 'qz8fryfa',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), unsplashImageAsset()],
 
   schema: {
     types: schemaTypes,
