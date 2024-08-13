445b0ff36034c53b71d36e539c621fbe4a264a52
barel31 <barel123133@gmail.com>
2024-08-13 14:25:57 +0300

    refactor: Move AccordionQA, Carousel, and image logic from Content to ContentBody

--- frontend/components/Content/index.tsx
+++ frontend/components/Content/index.tsx
@@ -1,7 +1,4 @@
 import { notFound } from 'next/navigation';
-import { urlFor } from '@/client';
-import AccordionQA from './Accordion';
-import Carousel from '../Carousel';
 import BackgroundImage from './BackgroundImage';
 import ContentBody from './ContentBody';
 import ContentHeader from './ContentHeader';
@@ -11,15 +8,11 @@ function Content({ route, contact }: { route: Route; contact?: ContactInfo }) {
 
   const slug = route?.slug?.current;
 
-  const sliders = route?.images?.map(image => urlFor(image).dpr(2).url());
-
   return (
     <div className="content m-auto text-center">
       <BackgroundImage route={route} />
       <ContentHeader route={route} />
       <ContentBody route={route} contact={contact} slug={slug} />
-      {route?.qAndA && <AccordionQA qa={route.qAndA} />}
-      {sliders && <Carousel images={sliders!} />}
     </div>
   );
 }
