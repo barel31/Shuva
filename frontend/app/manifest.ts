7207a9c70d868503d69faa442fd61a18b8f1bb30
barel31 <barel123133@gmail.com>
2024-09-03 12:52:32 +0300

    fix: update manifest file to fix incorrect paths

--- frontend/app/manifest.ts
+++ frontend/app/manifest.ts
@@ -5,19 +5,18 @@ export default function manifest(): MetadataRoute.Manifest {
     name: process.env.NEXT_PUBLIC_SITE_NAME!,
     short_name: process.env.NEXT_PUBLIC_SITE_NAME_SHORT!,
     description: process.env.NEXT_PUBLIC_SITE_DESCRIPTION!,
-    icons: [
-      {
-        src: 'favicon.ico',
-        sizes: '256x256',
-        type: 'image/x-icon',
-        purpose: 'any',
-      },
-    ],
-    start_url: '.',
+    start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#000000',
     id: '/manifest.webmanifest',
     orientation: 'any',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: '256x256',
+        type: 'image/x-icon',
+      },
+    ],
   };
 }
